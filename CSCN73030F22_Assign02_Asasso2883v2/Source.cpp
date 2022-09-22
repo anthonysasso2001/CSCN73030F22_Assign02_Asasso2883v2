b0c463291018df001bb5ea0991285fb7729f58de
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2022-09-22 09:21:41 -0400

    Add project files.

---
+++ CSCN73030F22_Assign02_Asasso2883v2/Source.cpp
@@ -0,0 +1,12 @@
+//By: Anthony Sasso
+//Class: CSCN73030 - Fall, 2022
+//Created: 2022/09/21
+	//Revision History
+		//See Github history under Git tab > View Branch History
+
+#include <iostream>
+
+int main(void) {
+
+	return 1;
+}
